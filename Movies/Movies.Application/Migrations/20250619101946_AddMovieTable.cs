c0c75bb9c3a41627b3509a9b6f845551a9723d3f
Yogesh Singh Mehra <yogeshwebdev1@gmail.com>
2025-06-19 15:51:48 +0530

    Add : table to db via migration have to install entityframeworkcore.design package in startup-project api

---
+++ Movies/Movies.Application/Migrations/20250619101946_AddMovieTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movie",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    year_of_release = table.Column<int>(type: "integer", nullable: false),
+                    slug = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_movie", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "movie");
+        }
+    }
+}
