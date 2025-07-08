f70b55a07bf4846f47f82f0695d2d7792f8e3cb7
Yogesh Singh Mehra <yogeshwebdev1@gmail.com>
2025-07-08 17:43:21 +0530

    Building result pattern

---
+++ Movies/Movies.Application/Shared/Foundation/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.Application.Shared.Foundation;
+
+public record Error(string Code, string Message, string? PropertyName = null)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
