41ed6f5f1a26b4679f69791d3aa344b87b8a77d7
Yogesh Singh Mehra <yogeshwebdev1@gmail.com>
2025-07-10 16:30:17 +0530

    Mod : GlobalExceptionHanlder remove validation Exception because it impacts the performance

--- Movies/Movies.Api/Middleware/GlobalExceptionHandler.cs
+++ Movies/Movies.Api/Middleware/GlobalExceptionHandler.cs
@@ -9,37 +9,6 @@ public class GlobalExceptionHandler : IExceptionHandler
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
         CancellationToken cancellationToken)
     {
-        if (exception is ValidationException validationException)
-        {
-            var validationErrors = validationException.Errors
-                .GroupBy(e => e.PropertyName)
-                .ToDictionary(g => g.Key,
-                    g => g.Select(e => e.ErrorMessage).ToArray());
-            
-            var validationProblemDetails = new ValidationProblemDetails
-            {
-                Title = "Validation Failure",
-                Status = StatusCodes.Status400BadRequest,
-                Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1", // link documentation for error if you have one.
-                Errors = validationErrors,
-                Instance = httpContext.Request.Path,
-                Extensions =
-                {
-                    ["traceId"] = httpContext.TraceIdentifier,
-                    ["method"] = httpContext.Request.Method
-                }
-            };
-
-            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            httpContext.Response.ContentType = "application/problem+json";
-            await httpContext.Response.WriteAsJsonAsync(validationProblemDetails, cancellationToken);
-            return true;
-        }
-
-        
-        // for all other server errors
-        // var error = exception.Message; for logging the errors.
-
         var problemDetails = new ProblemDetails
         {
             Title = "An unexpected error occurred.",
